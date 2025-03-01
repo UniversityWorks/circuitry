354b7b02e3c4470e3c9de75726d86079f7e3e7b8
Anton Popov <mffnrscl@gmail.com>
2025-03-01 17:12:54 +0200

    LA wOMAN

---
+++ assembly/lab1/hello.asm
@@ -0,0 +1,17 @@
+section .data
+    Message db 'First run of the assembly!', 10  
+    MsgLen equ $ - Message  
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1       
+    mov rdi, 1       
+    mov rsi, Message 
+    mov rdx, MsgLen  
+    syscall
+
+    mov rax, 60     
+    xor rdi, rdi    
+    syscall
